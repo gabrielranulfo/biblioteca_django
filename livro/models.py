69c05f04c9b3f60b2c5c9217c3fa10bdf60adfb4
gabrielranulfo <gabrielranulfo14@gmail.com>
2022-09-15 04:17:01 -0300

    Aula3

--- livro/models.py
+++ livro/models.py
@@ -1,6 +1,14 @@
 from tabnanny import verbose
 from django.db import models
 from datetime import date, datetime
+from usuarios.models import Usuario
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
 class Livros(models.Model):
     nome = models.CharField(max_length=100)
     autor = models.CharField(max_length=30)
@@ -11,7 +19,8 @@ class Livros(models.Model):
     data_emprestimo = models.DateTimeField(blank =True, null=True)
     data_devolucao = models.DateTimeField(blank=True, null=True)
     tempo_duracao = models.DateField(blank=True, null=True)
-
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)#se apagar a categoria nao apaga os livros dela
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
     class Meta:
         verbose_name = 'Livro'
         
--- livro/views.py
+++ livro/views.py
@@ -1,11 +1,17 @@
+from http.client import HTTPResponse
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
 from usuarios.models import Usuario
+from .models import Livros
 # Create your views here.
 
 def home(request):
     if request.session.get('usuario'):
-        usuario = Usuario.objects.get(id=request.session['usuario']).nome
-        return HttpResponse(f'Olá {usuario}')
+        usuario = Usuario.objects.get(id=request.session['usuario'])
+        livros = Livros.objects.filter(usuario=usuario)
+        return render(request,'home.html',{'livros':livros})
     else:
         return redirect('/auth/login/?status=2')
+
+def ver_livros(request,id):
+    livro = Livros.objects.get(id=id)
+    return render(request, 'ver_livro.html',{'livro':livro})
